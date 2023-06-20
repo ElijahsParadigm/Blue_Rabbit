f805abe4c78fc55a2d52c0b962a88e606c3a58fd
Elijah <ecapers721@gmail.com>
2023-06-19 20:44:41 -0400

    Added code.py

---
+++ code.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# #GREEN CAB COLUMN LIST
+# all_green_df = pd.read_csv ('/Users/elijahcapers/CIS4400_Class_Project/Blue_Ribbon/Pandas_Project/all_green_tripdata_2023.csv')
+# print(all_green_df.dtypes)
+# # green_column_headers = list(all_green_df.columns.values)
+# # print("The Column Header: ", green_column_headers)
+
+# # #YELLOW CAB COLUMN LIST
+# all_yellow_df = pd.read_csv ('/Users/elijahcapers/CIS4400_Class_Project/Blue_Ribbon/Pandas_Project/all_yellow_tripdata_2023.csv')
+# print(all_yellow_df.dtypes)
+# # yellow_column_headers = list(all_yellow_df.columns.values)
+# # print("The Column Header: ", yellow_column_headers)
+
+# #WEATHER DATA COLUMN LIST
+all_weather_df = pd.read_csv ('/Users/elijahcapers/CIS4400_Class_Project/Blue_Ribbon/Raw_Data/Weather/Weather_Raw_data.csv')
+print(all_weather_df.dtypes)
+# weather_column_headers = list(all_weather_df.columns.values)
+# print("The Column Header: ", weather_column_headers)
