910b32ae3c66bf91d44590941092a121cc552eb7
Elijah <ecapers721@gmail.com>
2023-06-26 12:05:00 -0400

    updated script

--- Yellow_Cab_Data.py
+++ Yellow_Cab_Data.py
@@ -4,31 +4,24 @@ import pandas as pd
 # data_2 = pd.DataFrame({'a': [1,2,3,4,5]})
 # print (data, data_2)
 
-# #Step 1
-# # To preview the raw data.
-january = pd.read_parquet ('/Users/elijahcapers/CIS4400_Class_Project/Blue_Ribbon/Raw_Data/Yellow/yellow_tripdata_2023-01.parquet', engine = 'auto')
-february = pd.read_parquet ('/Users/elijahcapers/CIS4400_Class_Project/Blue_Ribbon/Raw_Data/Yellow/yellow_tripdata_2023-02.parquet', engine = 'auto')
-march = pd.read_parquet ('/Users/elijahcapers/CIS4400_Class_Project/Blue_Ribbon/Raw_Data/Yellow/yellow_tripdata_2023-03.parquet', engine = 'auto')
+#Step 1
+# To preview the raw data.
+january = pd.read_parquet ('/Users/elijahcapers/Dropbox/Baruch College/Summer 23/CIS 4400 Data Warehousing for Analytics/Term Project/Raw_Data/Yellow/Parquet/yellow_tripdata_2023-01.parquet', engine = 'auto')
+february = pd.read_parquet ('/Users/elijahcapers/Dropbox/Baruch College/Summer 23/CIS 4400 Data Warehousing for Analytics/Term Project/Raw_Data/Yellow/Parquet/yellow_tripdata_2023-02.parquet', engine = 'auto')
+march = pd.read_parquet ('/Users/elijahcapers/Dropbox/Baruch College/Summer 23/CIS 4400 Data Warehousing for Analytics/Term Project/Raw_Data/Yellow/Parquet/yellow_tripdata_2023-03.parquet', engine = 'auto')
 
-# #DO NOT RUN THIS LINE OF CODE UNTIL APRIL DATA IS RELEASED AND IMPORTED
-# april = pd.read_parquet ('/Users/elijahcapers/CIS4400_Class_Project/Blue_Ribbon/Raw_Data/Yellow/yellow_tripdata_2023-04.parquet', engine = 'auto')
-
-print(january)
+# print(january)
 
 # #Step 2
 # # To convert the raw data from parquet to csv.
-# yellow_df_1 = pd.read_parquet ('/Users/elijahcapers/CIS4400_Class_Project/Blue_Ribbon/Raw_Data/Yellow/yellow_tripdata_2023-01.parquet', engine = 'auto')
+# yellow_df_1 = january
 # yellow_df_1.to_csv('yellow_tripdata_2023-01.csv')
 
-# yellow_df_2 = pd.read_parquet ('/Users/elijahcapers/CIS4400_Class_Project/Blue_Ribbon/Raw_Data/Yellow/yellow_tripdata_2023-02.parquet', engine = 'auto')
+# yellow_df_2 = february
 # yellow_df_2.to_csv('yellow_tripdata_2023-02.csv')
 
-# yellow_df_3 = pd.read_parquet ('/Users/elijahcapers/CIS4400_Class_Project/Blue_Ribbon/Raw_Data/Yellow/yellow_tripdata_2023-03.parquet', engine = 'auto')
+# yellow_df_3 = march
 # yellow_df_3.to_csv('yellow_tripdata_2023-03.csv')
-
-# #DO NOT RUN THIS LINE OF CODE UNTIL APRIL DATA IS RELEASED AND IMPORTED
-# yellow_df_4 = pd.read_parquet ('/Users/elijahcapers/CIS4400_Class_Project/Blue_Ribbon/Raw_Data/Yellow/yellow_tripdata_2023-04.parquet', engine = 'auto')
-# yellow_df_4.to_csv('yellow_tripdata_2023-04.csv')
 
 # # Step 3
 # #Need to merge all files using concatenation.
